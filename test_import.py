63ca0784de0df9ab67d58f4b78de03731f3fa301
scalgary <57383694+scalgary@users.noreply.github.com>
2025-07-28 17:08:13 +0000

    add more

--- test_import.py
+++ test_import.py
@@ -1,6 +1,16 @@
-import selenium
-import webdriver_manager
-import dotenv
-import PIL
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
+import os
+import time
+from PIL import Image
+
 
 print("All dependencies imported successfully!")
