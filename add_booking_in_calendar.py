c2619f8a6b481f8808277e6ae8363330c74579d1
scalgary <57383694+scalgary@users.noreply.github.com>
2025-10-23 20:33:45 +0000

    calendar in main

--- add_booking_in_calendar.py
+++ add_booking_in_calendar.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 import icalendar
 
 # Configuration
-load_dotenv()
+#load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
